a94a5ca04b56a2ef827edd6ce1b5325a35204dfe
Hunnyken <138400053+Hunnyken@users.noreply.github.com>
2023-07-02 16:41:57 -0300

    Add files via upload

---
+++ teste.py
@@ -0,0 +1,48 @@
+trabalho = int(input('Qual a nota do trablaho?\nR:'))
+prova = int(input('Qual a nota da prova?\nR:'))
+media = (trabalho + prova) /2
+if(media <= 5):
+    print('REPROVaaaado')
+else:
+    print('provdo')
+
+    
+
+    
+# operadores aritmeticos:
+# + - soma
+# - - subtração
+# * = multplicação
+# / - divisao
+# // - sobra na divisã
+
+# tipos primitivos :
+# float - números com vírgula
+# int - números naturais
+# str - string(texto)
+
+# dados avançado
+# lista [] - armazena um conjunto de valores , para pegar um especifico só digitar nome_da_lista[indice]
+
+# comandos:
+# print - imprimir + f'{variavel}'
+# input - entrada de dados + \n() para pular linha
+
+# estrutura de repetições:
+# for():
+# while():
+
+#estrutura condifional:
+# if():
+#else:
+
+# --------- #
+# variavel - nome_da_variavel = valor
+
+#operadores lógicos:
+# < - menor
+# > - maior
+# == - igual
+# != - diferente
+# <= - menor ou igual
+# >= - maior ou igual
