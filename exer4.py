67c6604e4affde3c184fb7a30393448d1421fe0d
Hunnyken <138400053+Hunnyken@users.noreply.github.com>
2023-07-10 17:37:53 -0300

    Add files via upload

---
+++ exer4.py
@@ -0,0 +1,8 @@
+n = int(input('Digite um numero para saber seu antecessor e seu sucessor:'))
+print('O sucessor de {} é {}, e seu antecessor é {}'.format(n, (n + 1), (n - 1)))
+
+
+#n = int(input('Digite um numero para sabermos seu sucessor e seu antecessor:'))
+#s = n + 1
+#a = n - 1
+#print('O sucessor de {} é {}, e seu antecessor é {}.'.format(n, s, a))
