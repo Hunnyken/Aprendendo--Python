fbc401b9c228f7ebed09d8d8cd73e9ff726fa0f0
Hunnyken <138400053+Hunnyken@users.noreply.github.com>
2023-11-24 01:27:47 -0300

    Update 023.py

--- 023.py
+++ 023.py
@@ -1,4 +1,4 @@
-ano = int(input('Digite o ano para descobrir se le é um ano bissexto ou não:'))
+ano = int(input('Digite o ano para descobrir se ele é um ano bissexto ou não:'))
 if ano % 4 == 0 and ano % 100 != 0 or ano % 400 == 0:
     print('O ano {} é um ano bissexto!'.format(ano))
 else:
