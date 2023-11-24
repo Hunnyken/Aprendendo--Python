e7ce79872f985eb904ac8cd76e80e7310e9251ee
Hunnyken <138400053+Hunnyken@users.noreply.github.com>
2023-11-24 01:44:00 -0300

    Update 026.py
    correção de código

--- 026.py
+++ 026.py
@@ -2,14 +2,12 @@ def menu():
     r1 = float(input('Digite o valor da primeira reta:'))
     r2 = float(input('Digite o valor da segunda reta:'))
     r3 = float(input('Digite o valor da terceira reta:'))
-    soma = r1 + r2
-    if soma > r3:
+    if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r2:
         print()
-        print('Com esses valores não é possível fazer um triangulo.')
+        print('Com esses valores é possível fazer um triangulo.')
     else:
         print()
-        print('Com essses valores é possível de se fazer um triangulo.')
+        print('Com essses valores não é possível de se fazer um triangulo.')
     print()
 while True:
     menu()
-
