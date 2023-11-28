c57f5917b8f06edbd859311b10118e5ad0789a9f
Hunnyken <138400053+Hunnyken@users.noreply.github.com>
2023-11-28 07:42:15 -0300

    Update 030.py
    Correção do código

--- 030.py
+++ 030.py
@@ -1,9 +1,10 @@
 from datetime import date
-ano = int(input('Em que ano você nasceu?'))
+ano = int(input('Em que ano você nasceu? '))
 ano1 = date.today().year
-if  ano1 - ano < 18:
-    print('Voê precisará se alistar nos próximo(s) ano(s).')
-elif ano1 - ano == 18:
-    print('Você se alistará ainda esse ano.')
+ano_mes = date.today().month
+if  ano1 - ano < 18 and ano_mes <= 12:
+    print('Voê precisará se alistar nos próximo(s) {} ano(s) e {} mês(es).'.format(ano1 - ano ,12 - ano_mes))
+elif ano1 - ano == 18 and ano_mes == 12:
+    print('Você deverá se alistar AGORA!.')
 elif ano1 - ano > 18:
-    print('Você já passou do tempo de alistamento, em caso de não alistamento, deverá pagar uma multa.')
+    print('Você já passou {} anos e {} meses do tempo de alistamento, você deverá pagar uma multa.'.format((ano1 - ano) - 18, ano_mes))
