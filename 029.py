3e8dbe464c30b4c194260de95169bd9c7cd65092
Hunnyken <138400053+Hunnyken@users.noreply.github.com>
2023-11-28 04:42:24 -0300

    Selecionando o maior número digitado pelo usuário.

---
+++ 029.py
@@ -0,0 +1,6 @@
+n1 = int(input('Digite um numero:'))
+n2 = int(input('Digite o segundo numero:'))
+if n1 > n2:
+    print('O número {} é maior que o {}'.format(n1, n2))
+else:
+    print('O número {} é menor que {}'.format(n1, n2))
