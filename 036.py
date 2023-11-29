d3c15d5bd6843fffb5fc8885d144d57d3cc6450c
Hunnyken <138400053+Hunnyken@users.noreply.github.com>
2023-11-29 04:59:21 -0300

    Pedra, papel e tesoura

---
+++ 036.py
@@ -0,0 +1,30 @@
+import random
+
+def jogo_pedra_papel_tesoura(escolha_jogador):
+    escolhas = ['pedra', 'papel', 'tesoura']
+    escolha_computador = random.choice(escolhas)
+
+    print(f'Você escolheu: {escolha_jogador}')
+    print(f'O computador escolheu: {escolha_computador}')
+
+    if escolha_jogador == escolha_computador:
+        return 'Empate!'
+    elif (  
+        (escolha_jogador == 'pedra' and escolha_computador == 'tesoura') or
+        (escolha_jogador == 'papel' and escolha_computador == 'pedra') or
+        (escolha_jogador == 'tesoura' and escolha_computador == 'papel')
+    ):
+        return 'Você venceu!'
+    else:
+        return 'Você perdeu!'
+
+# Solicitar escolha do jogador
+escolha_usuario = input('Escolha pedra, papel ou tesoura: ').lower()
+
+# Verificar se a escolha do jogador é válida
+if escolha_usuario not in ['pedra', 'papel', 'tesoura']:
+    print('Escolha inválida. Por favor, escolha pedra, papel ou tesoura.')
+else:
+    # Executar o jogo e imprimir o resultado
+    resultado = jogo_pedra_papel_tesoura(escolha_usuario)
+    print(resultado)
