c063572f0640d4f9413d038a1692d0028d9539cc
Hunnyken <138400053+Hunnyken@users.noreply.github.com>
2023-11-29 04:58:19 -0300

    Add files via upload

---
+++ 033.py
@@ -0,0 +1,19 @@
+def menu():
+    r1 = float(input('Digite o valor da primeira reta:'))
+    r2 = float(input('Digite o valor da segunda reta:'))
+    r3 = float(input('Digite o valor da terceira reta:'))
+    if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r2:
+        print()
+        print('Com esses valores é possível fazer um triangulo.')
+        if r1 == r2 and r1 == r3:
+            print('Triângulo Equilatero.')
+        elif r1 == r2 != r3 or r3 == r1 != r2:
+            print('Triangulo Isosceles')
+        elif r1 != r2 != r3 != r1:
+            print('Triangulo Escaleno')
+    else:
+        print()
+        print('Com essses valores não é possível de se fazer um triangulo.')
+    print()
+while True:
+    menu()
