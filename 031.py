c161e06fdd8d923a84601696f70eaedc18650d9f
Hunnyken <138400053+Hunnyken@users.noreply.github.com>
2023-12-02 19:58:01 -0300

    Update 031.py

--- 031.py
+++ 031.py
@@ -3,7 +3,7 @@ n2 = float(input('Digite a sua primeira nota: '))
 n3 = float(input('Digite a sua primeira nota: '))
 media = (n1 + n2 + n3) / 3
 if media >= float(7):
-    print(f'Você teve a media de {media:.1f} e foi aprovado! PARABÉNS!')
+    print(f'Você teve a media de {media:.1f} e foi aprovado! PARABÉNS!!!')
 elif media >= 5.1 and media <= 6.9:
     print(f'Você teve a média de {media:.1f} e está de recuperação!')
 else:
