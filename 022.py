913e885dba992bd5bf7f004b89c3f356825eaa13
Hunnyken <138400053+Hunnyken@users.noreply.github.com>
2023-12-05 14:57:24 -0300

    Update 022.py
    Reformatação f string

--- 022.py
+++ 022.py
@@ -1,5 +1,5 @@
 viagem = int(input('Digite a distância em Km da sua viagem:'))
 if viagem <= 200:
-    print('A passagem ficará em R${}'.format(viagem * 0.50))
+    print(f'A passagem ficará em R${viagem * 0.50}')
 else:
-    print('A passagem ficará em R${}'.format(viagem * 0.45))
+    print(f'A passagem ficará em R${viagem * 0.45}')
