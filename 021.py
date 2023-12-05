8476f645864f9e036bee4c252b7505cb084fb2d3
Hunnyken <138400053+Hunnyken@users.noreply.github.com>
2023-12-04 21:17:05 -0300

    Update 021.py
    Reformatação

--- 021.py
+++ 021.py
@@ -1,6 +1,6 @@
 n = int(input('Digite um número para saber se ele é ímpar ou par:'))
 conta = n % 2 
 if conta == 0:
-    print('O número {} é par!'.format(n))
+    print(f'O número {n} é par!')
 else:
-    print('O número {} é ímpar!'.format(n))
+    print(f'O número {n} é ímpar!')
