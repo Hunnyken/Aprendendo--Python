dabab61be47ac63f965f2be2c685704eb4eaa1ca
Hunnyken <138400053+Hunnyken@users.noreply.github.com>
2023-12-04 21:14:21 -0300

    Update 001.py
    reformatação

--- 001.py
+++ 001.py
@@ -2,4 +2,4 @@ valor = float(input('Digite o valor em que será aplicado o desconto:'))
 por = int(input('Digite o valor da porcentagem de desconto:'))
 n = (valor * por) / 100
 x = valor - n
-print('O valor de R${} com {}% de desconto resultará em R${} de desconto. O resultado ficou em R${} depois dos descontos.'.format(valor, por, n, x))
+print(f'O valor de R${valor} com {por}% de desconto resultará em R${n} de desconto. O resultado ficou em R${x} depois dos descontos.')
