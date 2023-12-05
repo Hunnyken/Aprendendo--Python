9207b909acb4ec26bc992cbbf17b26e0c2f62c8b
Hunnyken <138400053+Hunnyken@users.noreply.github.com>
2023-12-04 21:15:08 -0300

    Update 002.py
    Reformatação

--- 002.py
+++ 002.py
@@ -1,3 +1,3 @@
 f = float(input('Digite quantos °C está em sua cidade para ser convertido em Fahrenheit:'))
 conta = ((f*9)/ 5) + 32 
-print('{:.1f}°C é igual à {:.1f} Fahrenheit.'.format(f, conta))
+print(f'{f:.1f}°C é igual à {conta:.1f} Fahrenheit.')
