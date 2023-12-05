e5ab1774b69afe72c066c5b4658b0451af7821f7
Hunnyken <138400053+Hunnyken@users.noreply.github.com>
2023-12-04 21:15:56 -0300

    Update 004.py
    Reformatação

--- 004.py
+++ 004.py
@@ -2,4 +2,4 @@ from math import sqrt
 op = float(input('Digite o cateto oposto do triangulo retangulo:'))
 ad = float(input('Digite o cateto adjascente do triangulo retangulo:'))
 num = sqrt(op * op + ad * ad)
-print('a hipotenusa do triangulo retangulo é igual a {}'.format(num))
+print(f'a hipotenusa do triangulo retangulo é igual a {num}')
