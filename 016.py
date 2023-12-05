02761b537218b43643092abd3f020d131519f8e2
Hunnyken <138400053+Hunnyken@users.noreply.github.com>
2023-12-05 15:01:13 -0300

    Update 016.py
    Reformatação para f string

--- 016.py
+++ 016.py
@@ -1,8 +1,8 @@
 nome = input('Digite seu nome:').strip()
 div = nome.split()
-print('Bem-vindo {}!'.format(nome))
-print('Seu primeiro nome é {}'.format(div[0]))
-print('seu último nome é {}'.format(div[-1]))
+print(f'Bem-vindo {nome}!')
+print(f'Seu primeiro nome é {div[0]}')
+print(f'seu último nome é {div[-1]}')
 
 
 #outra forma de se fazer o ultimo topico é utilizando a função len() Ex: print('seu último nome é {}'.format(div[len(div)-1]))
