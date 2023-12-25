209a36b6b3a37693b0536fe05b0257a890c0f179
Hunnyken <138400053+Hunnyken@users.noreply.github.com>
2023-12-24 22:59:05 -0300

    Update exer3.py

--- exer3.py
+++ exer3.py
@@ -3,4 +3,4 @@ n2 = int(input('Digite outro valor:'))
 print('Os valores sao: a soma  {}, multiplicacao  {}, subtraçao {} e exponenciaçao {}'.format(n1 + n2, n1 * n2, n1 - n2, n1 ** n2), end='')
 print(' a divisao {:.3f}, a sobra {}, a divisao inteira {}'.format(n1 / n2, n1 % n2, n1 // n2))
 
-#No exercicio foi usada muitas linhas para facer as somas, subtraçoes, divisoes e etc, entao fis de uma maneira onde tudo poderia ser feito em uma linha ou duas.
+#No exercicio foram usadas muitas linhas para facer as somas, subtraçoes, divisoes e etc, entao fiz de uma maneira onde tudo poderia ser feito em uma linha ou duas.
